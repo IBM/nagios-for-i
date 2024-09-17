24320ae7b56aa2065da168c31952d6141407dd5b
Andrew Holman <Andrew.Holman@ibm.com>
2024-09-17 17:07:42 -0500

    Cleanup shell commands.
    Signed-off-by: Andrew Holman <Andrew.Holman@ibm.com>

--- Wizard for NagiosXI/ibmiservice/plugins/server_restart.sh
+++ Wizard for NagiosXI/ibmiservice/plugins/server_restart.sh
@@ -4,7 +4,7 @@ service nagios stop
 echo "Nagios Server stopped"
 
 pid=$(ps -ef | grep java | grep "name=nagios" | awk '{print $2}')
-if ["" = "$pid"] ; then
+if [ "" = "$pid" ] ; then
 	echo "Starting Nagios Server"
 	nohup java -cp /usr/local/nagios/libexec/jt400.jar:/usr/local/nagios/libexec/gson.jar:/usr/local/nagios/libexec/nagios4i.jar com.ibm.nagios.Server -dname=nagios  > /usr/local/nagios/var/server.log &
 	service nagios start
